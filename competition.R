d73da5a42668f4361a963ff50d4c5cea34156ee2
bgulbis <brian.gulbis@gmail.com>
2016-05-26 10:11:02 -0500

    second model - make dummy vars

--- competition.R
+++ competition.R
@@ -1,89 +1,49 @@
 # competition
 
 library(readr)
+library(plyr)
 library(dplyr)
+library(tibble)
+library(stringr)
 library(caret)
 
 library(doParallel)
 registerDoParallel()
 
 # get training and testing data
-train <- read_csv("train2016.csv") %>%
-    mutate_each(funs(factor), -USER_ID, -YOB)
+training <- read_csv("train2016.csv") %>%
+    mutate(Income = str_replace_all(Income, "\\$|,[0-9]{3}", ""))
+    # mutate_each(funs(factor), -USER_ID, -YOB)
 
-# check for incorrect data in YOB
-summary(train$YOB)
-tmp <- filter(train, YOB < 1940 | YOB > 2000)
-
-
-test <- read_csv("test2016.csv") %>%
-    mutate_each(funs(factor), -USER_ID, -YOB)
+testing <- read_csv("test2016.csv") %>%
+    mutate(Income = str_replace_all(Income, "\\$|,[0-9]{3}", ""))
+    # mutate_each(funs(factor), -USER_ID, -YOB)
 
 # create validation set from training data
-inTrain <- createDataPartition(y = train$Party, p = 0.75, list = FALSE)
-train.data <- train[inTrain, ]
-valid.data <- train[-inTrain, ]
+inTrain <- createDataPartition(y = training$Party, p = 0.75, list = FALSE)
+train.data <- training[inTrain, ]
+valid.data <- training[-inTrain, ]
 
 # preproccess data -------------------------------------
-train.set <- dplyr::select(train.data, -USER_ID, -Party)
-valid.set <- dplyr::select(valid.data, -USER_ID, -Party)
-test.set <- dplyr::select(test, -USER_ID)
-
-# how many complete cases
-# comp <- complete.cases(train.set)
-# sum(comp)
-
-# impute missing data
-library(mice)
-# miss <- md.pattern(train.set)
-# miss <- md.pairs(train.set)
-
-# use mice to impute missing values
-
-get_impute <- function(file.save, set, method = NULL) {
-    if (file.exists(file.save)) {
-        imp <- readRDS(file.save)
-    } else {
-        if(!is.null(method)) {
-            imp <- mice(set, method = method)
-        } else {
-            imp <- mice(set)
-        }
-        saveRDS(imp, file.save)
-    }
-    imp
-}
-
-train.imp <- get_impute("train_imp.Rds", train.set)
-train.comp <- complete(train.imp)
-
-valid.imp <- get_impute("valid_imp.Rds", valid.set, train.imp$method)
-valid.comp <- complete(valid.imp)
-
-test.imp <- get_impute("test_imp.Rds", test.set, train.imp$method)
-test.comp <- complete(test.imp)
-
-# check for near-zero covariates
-# near.zero.fields <- nearZeroVar(train.set, saveMetrics = TRUE)
-# cond <- checkConditionalX(train.set, levels(train.set$Q124742))
-# near.zero <- nearZeroVar(train.set)
-# train.set <- train.set[, -near.zero]
-
-# remove highly-correlated variables - doesn't work for categorical data
-# train.cor <- cor(train.set, use = "complete.obs")
-# cor(train.set$Gender, train.set$Income)
+train.set <- select(train.data, -USER_ID, -Party)
+train.dv <- dummyVars(~ ., data = train.set)
+train.dvp <- predict(train.dv, newdata = train.set) %>% as_data_frame()
 
 set.seed(1235)
-pre.proc <- preProcess(train.comp, outcome = train.data$Party,
-                       method = c("conditionalX"))
+pre.proc <- preProcess(train.dvp, outcome = train.data$Party,
+                       method = c("knnImpute"))
 
-train.comp <- predict(pre.proc, train.comp)
-train.comp$Party <- train.data$Party
+train.proc <- predict(pre.proc, train.dvp)
+train.proc$Party <- train.data$Party
 
-valid.comp <- predict(pre.proc, valid.comp)
-valid.comp$Party <- valid.data$Party
+valid.set <- select(valid.data, -USER_ID, -Party)
+valid.dvp <- predict(train.dv, newdata = valid.set) %>% as_data_frame()
+valid.proc <- predict(pre.proc, valid.dvp)
+valid.proc$Party <- valid.data$Party
 
-test.comp <- predict(pre.proc, test.comp)
+test.set <- select(testing, -USER_ID)
+test.dvp <- predict(train.dv, newdata = test.set) %>% as_data_frame()
+test.proc <- predict(pre.proc, test.dvp)
 
 # random forest ----------------------------------------
 
@@ -95,39 +55,45 @@ myseeds <- list(c(123,234,345), c(456,567,678), c(789,890,901),
 trCtrl <- trainControl(method="repeatedcv", seeds=myseeds)
 
 library(randomForest)
-modelRF <- train(Party ~ ., method="rf", data=train.comp, trControl=trCtrl)
+
+mod <- "modelRF.Rds"
+if(file.exists(mod)) {
+    modelRF <- readRDS(mod)
+} else {
+    modelRF <- train(Party ~ ., method="rf", data=train.proc, trControl=trCtrl)
+    saveRDS(modelRF, mod)
+}
 
 # boosting ---------------------------------------------
 
 library(gbm)
-modelGBM <- train(Party ~ ., method="gbm", data=train.comp, verbose=FALSE,
+modelGBM <- train(Party ~ ., method="gbm", data=train.proc, verbose=FALSE,
                   trControl=trCtrl)
 
 # lda --------------------------------------------------
 
-modelLDA <- train(Party ~ ., method="lda", data=train.comp, trControl=trCtrl)
+modelLDA <- train(Party ~ ., method="lda", data=train.proc, trControl=trCtrl)
 
 # confusion matrix -------------------------------------
 
-predRF <- predict(modelRF, newdata = valid.comp)
-cmRF <- confusionMatrix(predRF, valid.comp$Party)
+predRF <- predict(modelRF, newdata = valid.proc)
+cmRF <- confusionMatrix(predRF, valid.data$Party)
 
-predGBM <- predict(modelGBM, newdata = valid.comp)
-cmGBM <- confusionMatrix(predGBM, valid.comp$Party)
+predGBM <- predict(modelGBM, newdata = valid.proc)
+cmGBM <- confusionMatrix(predGBM, valid.data$Party)
 
-predLDA <- predict(modelLDA, newdata = valid.comp)
-cmLDA <- confusionMatrix(predLDA, valid.comp$Party)
+predLDA <- predict(modelLDA, newdata = valid.proc)
+cmLDA <- confusionMatrix(predLDA, valid.data$Party)
 
-predDF <- data.frame(predRF, predGBM, predLDA, Party = valid.comp$Party)
+predDF <- data.frame(predRF, predGBM, predLDA, Party = valid.data$Party)
 modStack <- train(Party ~ ., data = predDF, method = "rf")
-predStack <- predict(modStack, newdata = valid.comp)
-cmStack <- confusionMatrix(predStack, valid.comp$Party)
+predStack <- predict(modStack, newdata = valid.proc)
+cmStack <- confusionMatrix(predStack, valid.data$Party)
 
-# out of sample error ----------------------------------
+# test data --------------------------------------------
 
+predTest <- predict(modStack, newdata = test.proc)
 
-# Model | Out of Sample Error
-# ------|------------------------------
-#     Random Forest | `r round((1 - cmRF$overall["Accuracy"]) * 100, 2)`%
-# Boosting | `r round((1 - cmGBM$overall["Accuracy"]) * 100, 2)`%
-# Linear Discriminate Analysis | `r round((1 - cmLDA$overall["Accuracy"]) * 100, 2)`%
+test.submit <- data_frame(USER_ID = test.data$USER_ID, Predictions = predTest)
+
+write_csv(test.submit, "submission.csv")
