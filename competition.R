39c3ea10ce259ed18be7911ca474fd7121889d68
bgulbis <brian.gulbis@gmail.com>
2016-05-26 13:49:12 -0500

    bagging instead of boosting

--- competition.R
+++ competition.R
@@ -31,21 +31,21 @@ train.dvp <- predict(train.dv, newdata = train.set) %>% as_data_frame()
 
 set.seed(1235)
 pre.proc <- preProcess(train.dvp, outcome = train.data$Party,
-                       method = c("knnImpute"))
+                       method = "knnImpute")
 
 train.proc <- predict(pre.proc, train.dvp)
-train.proc$Party <- train.data$Party
+train.result <- as.factor(train.data$Party)
 
 valid.set <- select(valid.data, -USER_ID, -Party)
 valid.dvp <- predict(train.dv, newdata = valid.set) %>% as_data_frame()
 valid.proc <- predict(pre.proc, valid.dvp)
-valid.proc$Party <- valid.data$Party
+valid.result <- as.factor(valid.data$Party)
 
 test.set <- select(testing, -USER_ID)
 test.dvp <- predict(train.dv, newdata = test.set) %>% as_data_frame()
 test.proc <- predict(pre.proc, test.dvp)
 
-# random forest ----------------------------------------
+# individual models ------------------------------------
 
 myseeds <- list(c(123,234,345), c(456,567,678), c(789,890,901),
                 c(987,876,765), c(654,543,432), c(321,210,109),
@@ -55,74 +55,60 @@ myseeds <- list(c(123,234,345), c(456,567,678), c(789,890,901),
 trCtrl <- trainControl(method = "repeatedcv", seeds = myseeds, classProbs = TRUE)
 
 library(randomForest)
-
-mod <- "modelRF.Rds"
-if(file.exists(mod)) {
-    modelRF <- readRDS(mod)
-} else {
-    modelRF <- train(Party ~ ., method="rf", data=train.proc, trControl=trCtrl)
-    saveRDS(modelRF, mod)
-}
-
-modelGLM <- train(Party ~ ., method = "glm", data = train.proc,
-                  trControl = trCtrl)
-
-# boosting ---------------------------------------------
-
 library(gbm)
-modelGBM <- train(Party ~ ., method = "gbm", data = train.proc, verbose = FALSE,
-                  trControl = trCtrl)
 
-# lda --------------------------------------------------
+# mod <- "modelRF.Rds"
+# if(file.exists(mod)) {
+#     modelRF <- readRDS(mod)
+# } else {
+#     modelRF <- train(x = train.proc, y = train.result, method="rf",
+#                      trControl=trCtrl)
+#     saveRDS(modelRF, mod)
+# }
 
-modelLDA <- train(Party ~ ., method = "lda", data = train.proc,
-                  trControl = trCtrl)
+# modelGLM <- train(x = train.proc, y = train.result, method = "glm",
+#                   trControl = trCtrl)
 
-# bagging ----------------------------------------------
+# modelGBM <- train(x = train.proc, y = train.result, method = "gbm",
+#                   verbose = FALSE, trControl = trCtrl)
 
-trCtrl2 <- trainControl(method = "none",
-                        classProbs = TRUE, allowParallel = FALSE)
+# modelLDA <- train(x = train.proc, y = train.result, method = "lda",
+#                   trControl = trCtrl)
 
-train.bag <- dplyr::select(train.proc, -Party)
-modelBag <- train(x = train.bag, y = as.factor(train.data$Party),
-                  method = "treebag", trControl = trCtrl)
+# modelBag <- train(x = train.proc, y = train.result, method = "treebag",
+#                   trControl = trCtrl)
 
-# svm --------------------------------------------------
-
-modelSVM <- train(Party ~ ., method = "svmRadial", data = train.proc,
-                  trControl = trCtrl)
+# modelSVM <- train(x = train.proc, y = train.result, method = "svmRadial",
+#                   trControl = trCtrl)
 
 # confusion matrix -------------------------------------
 
-predRF <- predict(modelRF, newdata = valid.proc)
-cmRF <- confusionMatrix(predRF, valid.data$Party)
-
-predGLM <- predict(modelGLM, newdata = valid.proc)
-cmGLM <- confusionMatrix(predGLM, valid.data$Party)
-
-predGBM <- predict(modelGBM, newdata = valid.proc)
-cmGBM <- confusionMatrix(predGBM, valid.data$Party)
-
-predLDA <- predict(modelLDA, newdata = valid.proc)
-cmLDA <- confusionMatrix(predLDA, valid.data$Party)
-
-predBag <- predict(modelBag, newdata = valid.proc)
-cmBag <- confusionMatrix(predBag, valid.data$Party)
-
-PredSVM <- predict(modelSVM, newdata = valid.proc)
-cmSVM <- confusionMatrix(PredSVM, valid.data$Party)
-
-# predDF <- data.frame(predRF, predGBM, predLDA, Party = valid.data$Party)
-# modStack <- train(Party ~ ., data = predDF, method = "rf")
-# predStack <- predict(modStack, newdata = valid.proc)
-# cmStack <- confusionMatrix(predStack, valid.data$Party)
+# predRF <- predict(modelRF, newdata = valid.proc)
+# cmRF <- confusionMatrix(predRF, valid.data$Party)
+#
+# predGLM <- predict(modelGLM, newdata = valid.proc)
+# cmGLM <- confusionMatrix(predGLM, valid.data$Party)
+#
+# predGBM <- predict(modelGBM, newdata = valid.proc)
+# cmGBM <- confusionMatrix(predGBM, valid.data$Party)
+#
+# predLDA <- predict(modelLDA, newdata = valid.proc)
+# cmLDA <- confusionMatrix(predLDA, valid.data$Party)
+#
+# predBag <- predict(modelBag, newdata = valid.proc)
+# cmBag <- confusionMatrix(predBag, valid.data$Party)
+#
+# PredSVM <- predict(modelSVM, newdata = valid.proc)
+# cmSVM <- confusionMatrix(PredSVM, valid.data$Party)
 
 # treebag, rpart, glm, knn, svmRadial
 
+# stacked model ----------------------------------------
+
 library(caretEnsemble)
 
-models <- caretList(Party ~ ., data = train.proc, trControl = trCtrl,
-                    methodList = c("glm", "gbm", "lda", "rf", "svmRadial"))
+models <- caretList(x = train.proc, y = train.result, trControl = trCtrl,
+                    methodList = c("glm", "treebag", "lda", "treebag", "rf", "svmRadial"))
 stack <- caretStack(models, method = "glm")
 
 predStack <- predict(stack, newdata = valid.proc)
