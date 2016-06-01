99ba617dbf8d9a3b0aa23859ea45f2249a4c1be2
bgulbis <brian.gulbis@gmail.com>
2016-06-01 08:29:48 -0500

    function to run multiple models

--- 2-model_tests.R
+++ 2-model_tests.R
@@ -20,49 +20,28 @@ trCtrl <- trainControl(method = "repeatedcv", repeats = 5, seeds = seeds,
                        index = createMultiFolds(train.party, 10, 5),
                        savePredictions = "final")
 
+# function to run multiple sets of models
+run_models <- function(model, data, prep = NULL, ctrl = trCtrl) {
+    require(caretEnsemble)
+    specs <- vector("list", length(prep))
 
-# run_pred <- function(model, data) {
-#     predict(model, newdata = data[, -1], na.action = na.pass)
-# }
-#
-# run_cm <- function(pred) {
-#     confusionMatrix(pred, valid.party)
-# }
-#
-# mods <- list(train.dv, train.hc)
-# mthd <- list("lda", "gbm")
-# prep <- c("knnImpute", "nzv")
-#
-# mod.cross <- list(x = mods, method = mthd, preProcess = prep)
-# cross.df <- cross_d(mod.cross)
-#
-# test <- invoke("train", mods, y = train.party, method = "gbm", trControl = ctrl, preProcess = "nzv")
-#
-# ctrl <- trainControl(method = "none", number = 1, repeats = 1)
-#
-# tries <- map(mods, ~ train(x = .x[, -1], y = train.party, trControl = ctrl, method = "lda", preProcess = "knnImpute"))
-# preds <- map(tries, predict)
-# glm1 <- train(x = train.set[, -1], y = train.party, method = "glm", trControl = trCtrl, metric = "ROC",
-#               preProcess = "knnImpute")
-#
-# try <- train(x = train.dv[, -1], y = train.party, trainControl = ctrl, method = "lda", preProcess = "knnImpute")
+    for (i in seq_along(specs)) {
+        specs[[i]] <- caretModelSpec(model, preProcess = prep[[i]])
+    }
 
-library(caretEnsemble)
+    names(specs) <- paste0(model, seq_along(specs))
 
-model <- "glm"
-prep <- list("knnImpute", c("nzv", "knnImpute"), c("nzv", "knnImpute", "pca"))
-specs <- vector("list", length(prep))
+    cl <- caretList(x = data, y = train.party, metric = "ROC",
+                    trControl = ctrl, tuneList = specs)
 
-for (i in seq_along(specs)) {
-    specs[[i]] <- caretModelSpec(model, preProcess = prep[[i]])
+    cm <- map(cl, predict, newdata = valid.set[, -1]) %>%
+        map(confusionMatrix, valid.party)
+
+    list(cl = cl, cm = cm)
 }
 
-names(specs) <- paste0("m", seq_along(specs))
 
-data <- train.set[, -1]
-cl <- caretList(x = data, y = train.party, metric = "ROC", trControl = trCtrl,
-                tuneList = specs)
+# modelCor(resamples(cl))
+# preds <- predict(cl, newdata = valid.set[, -1])
+# predResp <- map(cl, predict, newdata = valid.set[, -1]) %>% as_data_frame()
 
-modelCor(resamples(cl))
-preds <- predict(cl, newdata = valid.set[, -1])
-predResp <- map(cl, predict, newdata = valid.set[, -1]) %>% as_data_frame()
